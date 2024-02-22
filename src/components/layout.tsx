d9948d813e5bcb580a93e3ea3fcc21548a86efa6
namoong0917 <namoong0917@gmail.com>
2024-02-23 05:51:03 +0900

    svg에 aria-label 추가, 함수명 변경

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -14,24 +14,18 @@ const Wrap = styled.div`
 
 const MenuWrap = styled.nav``;
 
-const Menu = styled.ul`
+const MenuUl = styled.ul`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 20px;
-  &.log-out {
-    border: tomato;
-  }
 `;
 
-const List = styled.li``;
-
-const MenuItem = styled.span`
+const MenuList = styled.li`
   cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
-  /* border: 2px solid #fff; */
   height: 50px;
   width: 50px;
   border-radius: 50%;
@@ -39,6 +33,11 @@ const MenuItem = styled.span`
   svg {
     width: 30px;
     stroke: #864622;
+  }
+  &.log-out {
+    svg {
+      stroke: #d93e22;
+    }
   }
 `;
 
@@ -56,50 +55,11 @@ export default function Layout() {
   return (
     <Wrap>
       <MenuWrap>
-        <Menu>
-          <List>
+        <MenuUl>
+          <MenuList>
             <Link to="/">
-              <MenuItem>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                  />
-                </svg>
-              </MenuItem>
-            </Link>
-          </List>
-          <List>
-            <Link to="/profile">
-              <MenuItem>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-                  />
-                </svg>
-              </MenuItem>
-            </Link>
-          </List>
-          <List>
-            <MenuItem onClick={onLogOut} className="log-out">
               <svg
+                aria-label="Home"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -110,12 +70,48 @@ export default function Layout() {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
+                  d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
                 />
               </svg>
-            </MenuItem>
-          </List>
-        </Menu>
+            </Link>
+          </MenuList>
+          <MenuList>
+            <Link to="/profile">
+              <svg
+                aria-label="profile"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                />
+              </svg>
+            </Link>
+          </MenuList>
+          <MenuList onClick={onLogOut} className="log-out">
+            <svg
+              aria-label="log out"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
+              />
+            </svg>
+          </MenuList>
+        </MenuUl>
       </MenuWrap>
       <Outlet />
     </Wrap>
