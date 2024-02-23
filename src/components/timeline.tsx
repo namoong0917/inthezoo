8e0dbf80500c61392301b9bbfe87f11e038ee58f
namoong0917 <namoong0917@gmail.com>
2024-02-24 06:28:08 +0900

    Realtime Tweets(onSnapshot) - Firestore DB에서 실기간으로 쿼리의 변경 사항 수신

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,8 +1,16 @@
 import { useEffect, useState } from "react";
 import { styled } from "styled-components";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { db } from "../firebase";
 import Tweet from "./tweet";
+import { Unsubscribe } from "firebase/auth";
 
 export interface ITweet {
   id: string;
@@ -18,29 +26,49 @@ const Wrap = styled.ul``;
 
 export default function Timeline() {
   const [tweets, setTweet] = useState<ITweet[]>([]);
-  const fetchTweets = async () => {
-    const tweetsQuery = query(
-      collection(db, "tweets"),
-      orderBy("createdAt", "desc")
-    );
-    // 쿼리의 spanshot을 받아서
-    const snapshot = await getDocs(tweetsQuery);
-    const tweets = snapshot.docs.map((doc) => {
-      // 받은 모든 문서마다 개체를 만든다.
-      const { tweet, createdAt, userId, username, photo } = doc.data();
-      return {
-        tweet,
-        createdAt,
-        userId,
-        username,
-        photo,
-        id: doc.id,
-      };
-    });
-    setTweet(tweets);
-  };
+
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, "tweets"),
+        orderBy("createdAt", "desc"),
+        limit(25)
+      );
+      // 쿼리의 spanshot을 받아서
+      // const snapshot = await getDocs(tweetsQuery);
+      // const tweets = snapshot.docs.map((doc) => {
+      //   // 받은 모든 문서마다 개체를 만든다.
+      //   const { tweet, createdAt, userId, username, photo } = doc.data();
+      //   return {
+      //     tweet,
+      //     createdAt,
+      //     userId,
+      //     username,
+      //     photo,
+      //     id: doc.id,
+      //   };
+      // });
+      // onSnapshot은 특정 문서나 컬렉션, 쿼리 이벤트를 감지하여 realtime으로 이벤트콜백 함수를 실행해줄 수있다. 이를통해 db에 들어온 쿼리를 새로고침없이 화면에 반영할 수있다.
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          const { tweet, createdAt, userId, username, photo } = doc.data();
+          return {
+            tweet,
+            createdAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
+          };
+        });
+        setTweet(tweets);
+      });
+    };
     fetchTweets();
+    return () => {
+      unsubscribe && unsubscribe();
+    };
   }, []);
   return (
     <Wrap>
